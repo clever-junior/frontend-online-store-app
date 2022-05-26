52c40eeed460f089464d7c2ee5698fd8034fb5ec
HenriqueAmbrosano <henriqueambrosano@hotmail.com>
2022-05-26 18:41:20 -0300

    requisito 10

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -2,25 +2,77 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class ShoppingCart extends Component {
+  state = {
+    productsList: [],
+  }
+
+  componentDidMount() {
+    const productsList = JSON.parse(localStorage.getItem('itensDoCarrinho'));
+    this.setState({ productsList });
+  }
+
+  handleClick = ({ target }) => {
+    const { name, id } = target;
+    const productsList = JSON.parse(localStorage.getItem('itensDoCarrinho'));
+    const selectedProduct = productsList.find((product) => product.id === id);
+    if (name === 'minus') {
+      if (selectedProduct.quantidade === 1) {
+        selectedProduct.quantidade = 1;
+      } else {
+        selectedProduct.quantidade -= 1;
+      }
+    } else {
+      selectedProduct.quantidade += 1;
+    }
+    const index = productsList
+      .indexOf(productsList.find((element) => element.id === id));
+
+    productsList.splice(index, 1, selectedProduct);
+    this.setState({ productsList });
+    localStorage.setItem('itensDoCarrinho', JSON.stringify(productsList));
+  }
+
   render() {
-    const cartItems = JSON.parse(localStorage.getItem('itensDoCarrinho'));
+    const { productsList } = this.state;
     return (
       <div>
-        { (!cartItems)
+        { (!productsList)
           ? <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
           : (
             <div>
-              {cartItems.map((item) => (
+              {productsList.map((item) => (
                 <div key={ item.id }>
-                  <p data-testid="shopping-cart-product-name">
-                    { item.name }
-                  </p>
-                  <p>
-                    { item.price }
-                  </p>
-                  <p data-testid="shopping-cart-product-quantity">
-                    { item.quantidade }
-                  </p>
+                  <div>
+                    <p data-testid="shopping-cart-product-name">
+                      { item.name }
+                    </p>
+                    <p>
+                      { item.price }
+                    </p>
+                    <p data-testid="shopping-cart-product-quantity">
+                      { item.quantidade }
+                    </p>
+                  </div>
+                  <div>
+                    <button
+                      data-testid="product-decrease-quantity"
+                      type="button"
+                      id={ item.id }
+                      name="minus"
+                      onClick={ this.handleClick }
+                    >
+                      -
+                    </button>
+                    <button
+                      data-testid="product-increase-quantity"
+                      type="button"
+                      id={ item.id }
+                      name="add"
+                      onClick={ this.handleClick }
+                    >
+                      +
+                    </button>
+                  </div>
                 </div>
               ))}
             </div>
